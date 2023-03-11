2896af466f15e32af65ce53ebfdc6ad36f6ca9f9
Tom Risager <tom@stickleback.dk>
2023-03-11 14:02:53 +0100

    versions that raise on no result

--- lib/countries.ex
+++ lib/countries.ex
@@ -9,7 +9,7 @@ defmodule Countries do
   end
 
   @doc """
-  Returns the country with the given alpha-2 code.
+  Returns the country with the given alpha-2 code. Returns `nil` if no country
   """
   def get_by_alpha_2(alpha_2) do
     countries()
@@ -17,11 +17,25 @@ defmodule Countries do
   end
 
   @doc """
-  Returns the country with the given alpha-3 code.
+  Returns the country with the given alpha-2 code. Raises `ArgumentError` if no country
+  """
+  def get_by_alpha_2!(alpha_2) do
+    get_by_alpha_2(alpha_2) || raise(ArgumentError, "No country with alpha-2 code #{alpha_2}")
+  end
+
+  @doc """
+  Returns the country with the given alpha-3 code. Returns `nil` if no country
   """
   def get_by_alpha_3(alpha_3) do
     countries()
     |> Enum.find(fn country -> country.alpha_3 == alpha_3 end)
+  end
+
+  @doc """
+  Returns the country with the given alpha-3 code. Raises `ArgumentError` if no country
+  """
+  def get_by_alpha_3!(alpha_3) do
+    get_by_alpha_3(alpha_3) || raise(ArgumentError, "No country with alpha-3 code #{alpha_3}")
   end
 
   # -- Load countries from JSON once on compile time ---
--- test/countries_test.exs
+++ test/countries_test.exs
@@ -17,9 +17,33 @@ defmodule CountriesTest do
     end
   end
 
+  describe "get_by_alpha2!/2" do
+    test "returns the country with the given alpha-2 code" do
+      assert "Germany" = Countries.get_by_alpha_2!("DE") |> Map.get(:name)
+    end
+
+    test "raises an error if the country does not exist" do
+      assert_raise ArgumentError, "No country with alpha-2 code XX", fn ->
+        Countries.get_by_alpha_2!("XX")
+      end
+    end
+  end
+
   describe "get_by_alpha_3/1" do
     test "returns the country with the given alpha-3 code" do
       assert "Germany" = Countries.get_by_alpha_3("DEU") |> Map.get(:name)
+    end
+  end
+
+  describe "get_by_alpha_3!/1" do
+    test "returns the country with the given alpha-3 code" do
+      assert "Germany" = Countries.get_by_alpha_3!("DEU") |> Map.get(:name)
+    end
+
+    test "raises an error if the country does not exist" do
+      assert_raise ArgumentError, "No country with alpha-3 code XXX", fn ->
+        Countries.get_by_alpha_3!("XXX")
+      end
     end
   end
 end
